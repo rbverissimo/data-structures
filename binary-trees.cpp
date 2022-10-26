acfe4c1c762f7bc9121e8456f7817aa15670cc9d
rbverissimo <rverissimo.dev@gmail.com>
2022-10-26 12:10:10 -0300

    more on theory of binary trees and categorization

--- binary-trees.cpp
+++ binary-trees.cpp
@@ -9,3 +9,26 @@
 // the most common tree is the tree that any node can have 3 children max, also called the BINARY TREE
 
 
+struct Node {   // a node must store data and pointers to the leftmost and rightmost nodes; 
+	int data;
+	Node* left;
+	Node* right; 
+};
+
+// Strict binary trees: all nodes have either 2 or 0 children
+// Complete binary trees: all levels except possibly the last are completely filled and all nodes are as left as possible
+
+// Depth and level are synonyms
+// max number of nodes at level i = 2 ^ i 
+
+// Perfect Binary Tree = all levels are completely filled with a max number of nodes, so the number of nodes will be:
+// n = 2 ^ (i + 1) - 1   ---- this -1 is to discount for the root which is basically alone in its level otherwise it will be counted as 2 
+
+// to calculete the maxinum height of a given binary tree:  h = log(number of nodes + 1 ) base 2 - 1
+
+
+// the goal for efficiency in BigO is to keep the binary tree with less height as possible, thus the Balanced Binary Tree:
+
+// difference of height between left and right subtree height is no more than k (mostly 1) 
+
+// height of an empty tree = -1 , as the height of an 1 node tree = 0 
