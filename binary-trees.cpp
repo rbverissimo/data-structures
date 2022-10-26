1234934d1eee8c9f06c0d7555b5cdd66f379cb62
rbverissimo <rverissimo.dev@gmail.com>
2022-10-26 11:42:29 -0300

    updating theory as commmentary for trees properties

--- binary-trees.cpp
+++ binary-trees.cpp
@@ -1,2 +1,11 @@
 // introduction to trees
 // they are naturally hierarchical: a collection of entities called nodes linked together to simulate an hierarchy
+// we can only travel from node A to B in one direction: that's what hierarchy means after all
+// trees are a recursive data structure, the definition of one root with n childs defines a sub-tree which also contains a type of root
+// N nodes has n-1 edges 
+// Depth of x = length of the path from root to x, the lenght is calculated in edges basically
+// Height of x = x being the root of a tree or tree, its the longest path to a leaf (the furthermost leaf), being that a leaf will have 0 height
+// the height from the root of the tree to the last leaf is the total height of the tree
+// the most common tree is the tree that any node can have 3 children max, also called the BINARY TREE
+
+
